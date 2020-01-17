de65f3690270ee31b739b47a2292571de78bda9f
Rodrigo Viegas <rodrigoviegas@lojaspompeia.com.br>
2020-01-17 08:31:28 -0300

    'List'

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,3 +1,4 @@
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
@@ -8,3 +9,6 @@ class PontoTuristicoViewSet(ModelViewSet):
 
     def get_queryset(self):
         return PontoTuristico.objects.filter(status=True)
+
+    def list(self, request, *args, **kwargs):
+        return Response()
